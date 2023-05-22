78a6e2e68dab023892bf5ab7abd8850d70abc8d8
Waseque Arafat <107799688+boss42wa@users.noreply.github.com>
2023-05-22 18:19:38 +0600

    alhamdulliah great job bro

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@ const displayPhones = (phones, dataLimit) => {
   phonesContainer.textContent = "";
   // display 10 phones only
   const showAll = document.getElementById("show-all");
-  if (dataLimit && phones.length > 15) {
-    phones = phones.slice(0, 15);
+  if (dataLimit && phones.length > 10) {
+    phones = phones.slice(0, 10);
     showAll.classList.remove("d-none");
   } else {
     showAll.classList.add("d-hidden");
@@ -37,7 +37,9 @@ const displayPhones = (phones, dataLimit) => {
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text"> </p>
-                <button onclick="loadPhoneDetails(${phone.slug})" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+                <button onclick="loadModal('${phone.slug}')" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Details
+              </button>
                 
             </div>
         </div>
@@ -48,7 +50,7 @@ const displayPhones = (phones, dataLimit) => {
 };
 
 const processSearch = (dataLimit) => {
-  //   toggleSpinner(true);
+  // toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadPhones(searchText, dataLimit);
@@ -83,31 +85,40 @@ document.getElementById("btn-show-all").addEventListener("click", function () {
   processSearch();
 });
 
-const loadPhoneDetails = async (id) => {
-  const url = `www.openapi.programming-hero.com/api/phone/${id}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  displayPhoneDetails(data.data);
+// const loadPhoneDetails = async (id) => {
+//   const url = `www.openapi.programming-hero.com/api/phone/${id}`;
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   displayPhoneDetails(data.data);
+// };
+
+const loadModal = async (id) => {
+  console.log(id);
+  const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPhoneDetails(data.data));
 };
 
 const displayPhoneDetails = (phone) => {
   console.log(phone);
-  const modalTitle = document.getElementById("phoneDetailModalLabel");
+  const modalTitle = document.getElementById("exampleModalLabel");
   modalTitle.innerText = phone.name;
-  const phoneDetails = document.getElementById("phone-details");
+  const phoneDetails = document.getElementById("body");
   console.log(phone.mainFeatures.sensors[0]);
   phoneDetails.innerHTML = `
-        <p>Release Date: ${phone.releaseDate}</p>
-        <p>Storage: ${phone.mainFeatures}</p>
-        <p>Others: ${
-          phone.others ? phone.others.Bluetooth : "No Bluetooth Information"
-        }</p>
-        <p>Sensor: ${
-          phone.mainFeatures.sensors
-            ? phone.mainFeatures.sensors[0]
-            : "no sensor"
-        }</p>
-    `;
+  <img src="${phone.image}">
+          <p>Release Date: ${phone.releaseDate}</p>
+          <p>Storage: ${phone.mainFeatures}</p>
+          <p>Others: ${
+            phone.others ? phone.others.Bluetooth : "No Bluetooth Information"
+          }</p>
+          <p>Sensor: ${
+            phone.mainFeatures.sensors
+              ? phone.mainFeatures.sensors[0]
+              : "no sensor"
+          }</p>
+      `;
 };
-
-loadPhones("apple");
+loadPhones("iphone");
